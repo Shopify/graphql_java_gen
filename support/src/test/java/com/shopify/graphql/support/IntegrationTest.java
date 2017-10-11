84d155f17b2edd1e649dc5ab32b414ef5f994a20
Henry Tao <henry.tao@shopify.com>
2017-10-11 10:35:22 -0400

    Add optional to Input class

--- support/src/test/java/com/shopify/graphql/support/IntegrationTest.java
+++ support/src/test/java/com/shopify/graphql/support/IntegrationTest.java
@@ -163,7 +163,7 @@ public class IntegrationTest {
         String queryString = Generated.mutation(mutation -> mutation
             .setInteger(new Generated.SetIntegerInput("answer", 42).setTtl(null))
         ).toString();
-        assertEquals("mutation{set_integer(input:{key:\"answer\",value:42,ttl:null})}", queryString);
+        assertEquals("mutation{set_integer(input:{key:\"answer\",value:42})}", queryString);
     }
 
     @Test
